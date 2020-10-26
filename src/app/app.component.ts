8d7e5ba7e6d3eb43dbd81a41fb680c522d820172
Daniel-bin <daniel.coughlan@outlook.ie>
2020-10-26 13:25:30 +0000

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from "@angular/core";
+import { Title } from "@angular/platform-browser";
+import { WatchListService } from "./watch-list.service";
+
+@Component({
+  selector: "my-app",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
+})
+export class AppComponent implements OnInit {
+  watchList = [];
+  maxItemsShown = 4;
+
+  constructor(titleService: Title, private watchListService: WatchListService) {
+    titleService.setTitle(this.watchList.length + " items");
+  }
+
+  ngOnInit() {
+    this.watchListService
+      .getWatchList()
+      .subscribe(watchList => (this.watchList = watchList)); // this function returns an observable that contains the watchlist
+  }
+
+  moveItemUp(idx) {
+    let cur = this.watchList[idx];
+    let swap = this.watchList[idx - 1];
+    this.watchList[idx] = swap;
+    this.watchList[idx - 1] = cur;
+  }
+
+  moveItemDown(idx) {
+    let cur = this.watchList[idx];
+    let swap = this.watchList[idx + 1];
+    this.watchList[idx] = swap;
+    this.watchList[idx + 1] = cur;
+  }
+
+  showAll() {
+    this.maxItemsShown = this.watchList.length;
+  }
+}
